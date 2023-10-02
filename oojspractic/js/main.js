e7a467c760a3b285bb3a7b759216e9004cc6ea84
Kiril05 <143001326+Kiril05@users.noreply.github.com>
2023-10-03 01:32:22 +0200

    Add files via upload

--- oojspractic/js/main.js
+++ oojspractic/js/main.js
@@ -1,71 +1,112 @@
-class CheckingAccount {
-    constructor(name, balance) {
-        this.name = name;
-        this.balance = balance;
+
+function CheckingAccount(name, balance) {
+    this.name = name;
+    this.balance = balance;
+}
+
+CheckingAccount.prototype.deposit = function(amount) {
+    this.balance += amount;
+}
+CheckingAccount.prototype.withdraw = function(amount) {
+    if(this.balance - amount >= 0) {
+        this.balance -= amount;
     }
-    deposit(amount) {
-        this.balance += amount;
+}
+CheckingAccount.prototype.__str__ = function() {
+    return `CheckingAccount(name=${this.name},balance=${this.balance})`;
+}
+
+let checkDude = new CheckingAccount("Check Dude", 600);
+console.log(checkDude);
+
+
+function SavingAccount(name, balance, interest_rate) {
+    CheckingAccount.call(this, name, balance);
+    this.interest_rate = interest_rate;
+}
+
+SavingAccount.prototype.__proto__ = CheckingAccount.prototype;
+SavingAccount.prototype.apply_daily_interest = function() {
+    if(this.interest_rate > 0) {
+        this.balance += this.balance / 100 * this.interest_rate;
     }
-    withdraw(amount) {
-        if(this.balance - amount >= 0) {
-            this.balance -= amount;
+}
+SavingAccount.prototype.withdraw = function(amount) {
+        console.log("Can't withdraw");
+}
+
+let saveDude = new SavingAccount("Save Dude", 500, 30);
+console.log(saveDude);
+
+function CreditAccount(name, balance, interest_rate, credit_limit) {
+    SavingAccount.call(this, name, balance, interest_rate);
+    this.credit_limit = credit_limit;
+}
+CreditAccount.prototype.__proto__ = SavingAccount.prototype;
+CreditAccount.prototype.withdraw = function(amount) {
+    if(this.balance - amount >= 0 - this.credit_limit) {
+        this.balance -= amount;
+    } else {
+        console.log("You cross credit limit");
+    }
+}
+CreditAccount.prototype.apply_daily_interest = function() {
+    if(this.balance < 0) {
+        this.balance - this.balance / 100 * this.interest_rate;
+    }
+}
+let creditDude = new CreditAccount("Credit Dude", 1000, 20, 200);
+console.log(creditDude);
+
+function Bank() {
+    this.accounts = [];
+}
+Bank.prototype.openAccount = function(name, balance, interest_rate, credit_limit) {
+    let x = new CreditAccount(name, balance, interest_rate, credit_limit);
+    this.accounts.push(x);
+} 
+Bank.prototype.total_cash = function() {
+    let cash = 0;
+    for(let account in this.accounts) {
+        if (this.accounts[account]["balance"] > 0) {
+            cash += this.accounts[account]["balance"];
         }
     }
-    __str__() {
-        return `CheckingAccount(name=${this.name},balance=${this.balance})`;
+    return cash;
+}
+Bank.prototype.total_credit = function() {
+    let credit = 0;
+    for (let i = 0; i < this.accounts.length; i++) {
+        if (this.accounts[i]["balance"] < 0) {
+            credit += this.accounts[i]["balance"];
+        }
+    }
+    return credit;
+}
+
+Bank.prototype.apply_daily_interest = function(interest) {
+    for(let i = 0; i < this.accounts.length; i++) {
+        this.accounts[i]["interest_rate"] = interest;
+    }
+}
+Bank.prototype.display_accounts = function() {
+    for(let i = 0; i < this.accounts.length; i++) {
+        console.log(this.accounts[i]);
     }
 }
 
-class SavingAccount extends CheckingAccount {
-    constructor(name, balance, interest_rate) {
-        super(name, balance);
-        this.interest_rate = interest_rate;
-    }
-    withdraw() {
-        console.log("Can't withdraw from SavingAccount");
-    }
-    __str__() {
-        return `CheckingAccount(name=${this.name},interest_rate${this.interest_rate},balance=${this.balance})`;
-    }
-    apply_daily_interest() {
-        if(this.interest_rate > 0) {
-            this.balance + this.balance / 100 * this.interest_rate;
-        }
-    }
+let bank = new Bank;
+bank.openAccount("Tom", 500, 5, 1000);
+bank.openAccount("Jerry", 1000, 6, 3000);
+bank.openAccount("Cat", 1400, 300, 5000);
+console.log(bank.accounts);
+console.log(bank.total_cash());
+bank.apply_daily_interest(10);
+console.log(bank.accounts);
+
+for(let i = 0; i < bank.accounts.length; i++) {
+    bank.accounts[i].withdraw(1300);
 }
-
-class CreditAccount extends SavingAccount {
-    constructor(name, balance, interest_rate) {
-        super(name, balance, interest_rate);
-        this.credit_limit = this.credit_limit;
-    }
-    withdraw(amount) {
-        if(this.balance - amount >= 0 - this.credit_limit) {
-            this.balance -= amount;
-        }
-    }
-
-    apply_daily_interest() {
-        if(this.balance < 0) {
-            this.balance - this.balance / 100 * this.interest_rate;
-        }
-    }
-}
-
-class Bank {
-    constructor() {
-        this.accounts = [];
-    }
-    openCreditAccount(name, balance, interest_rate, credit_limit) {
-        this.accounts.push(new CreditAccount(name, balance, interest_rate, credit_limit));
-    }
-    display_accounts() {
-        for(let account in this.accounts) {
-            console.log(account);
-        }
-    }
-}
-
-Bank.openCreditAccount(tom, "Tom", 500, 5, 30);
-Bank.display_accounts();
+bank.display_accounts();
+console.log(bank.total_credit());
 
